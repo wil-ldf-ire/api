4e1c0dad12e8c263533cf9dbbe6ee485cdca0657
akshay / tw <akshay.wildfire@pm.me>
2021-05-30 23:26:23 +0530

    auth

--- v1/auth.php
+++ v1/auth.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type: application/vnd.api+json');
 
+$userpass = explode(':', base64_decode($authHeader[1]));
 $accessArray = $auth->getApiAccess($userpass[0], $userpass[1]);
 
 //If user has access, pass bearer token
