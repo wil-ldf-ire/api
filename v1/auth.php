a46cd31addb4be6f9949241579c2db3e15e4264b
akshay / tw <akshay.wildfire@pm.me>
2021-05-30 12:17:45 +0530

    authenticate user based on api_key and api_secret

--- v1/auth.php
+++ v1/auth.php
@@ -1,5 +1,14 @@
 <?php
 header('Content-Type: application/vnd.api+json');
-echo json_encode(($api->getRequestBody()));
+
+$api_auth_post_data = $api->getRequestBody();
+
+$user_id = $sql->executeSQL("SELECT `content`->'$.user_id' `user_id` FROM `data`
+    WHERE
+    `content`->'$.api_key'='" . $api_auth_post_data['api_key'] . "' &&
+    `content`->'$.api_secret'='" . $api_auth_post_data['api_secret'] . "' &&
+    `content`->'$.type'='api_key_secret'")[0]['user_id'];
+
+echo json_encode($dash->get_user($user_id));
 exit();
 ?>
