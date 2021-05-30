db18e54ba3feb14345ef499926bb1a8de580ebf8
akshay / tw <akshay.wildfire@pm.me>
2021-05-30 13:20:58 +0530

    type error correction

--- v1/auth.php
+++ v1/auth.php
@@ -1,16 +1,16 @@
 <?php
 header('Content-Type: application/vnd.api+json');
 
-$api_auth_post_data=$api->getRequestBody();
+$api_auth_post_data = $api->getRequestBody();
 
-$user_id = json_decode(
-    $sql->executeSQL( "
-        SELECT `content`->'$.user_id' `user_id` FROM `data`
-        WHERE
-        `content`->'$.api_key'='" . $api_auth_post_data['api_key'] . "' && 
-        `content`->'$.api_secret'='" . $api_auth_post_data['api_secret'] . "' && 
-        `content`->'$.type'='api_key_secret'")[0]['user_id'] );
+$user_id = (string) json_decode($sql->executeSQL("
+    SELECT `content`->'$.user_id' `user_id` FROM `data`
+    WHERE
+    `content`->'$.api_key'='" . $api_auth_post_data['api_key'] . "' &&
+    `content`->'$.api_secret'='" . $api_auth_post_data['api_secret'] . "' &&
+    `content`->'$.type'='api_key_secret'")[0]['user_id'], true);
 
-echo json_encode($dash->get_user($user_id););
+echo json_encode($dash->get_user($user_id));
+
 exit();
 ?>
